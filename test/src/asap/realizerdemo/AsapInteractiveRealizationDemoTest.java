11bf086b167524cf2f54226ebbdb10a1e053d98c
Daniel <d.p.davison@utwente.nl>
2020-02-25 14:12:50 +0100

    added LGPLv3 license notice and short blurb to all java files

--- test/src/asap/realizerdemo/AsapInteractiveRealizationDemoTest.java
+++ test/src/asap/realizerdemo/AsapInteractiveRealizationDemoTest.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2009-2020 Human Media Interaction, University of Twente, the Netherlands
+ *
+ * This file is part of the Articulated Social Agents Platform BML realizer (ASAPRealizer).
+ *
+ * ASAPRealizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ASAPRealizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ASAPRealizer.  If not, see http://www.gnu.org/licenses/.
+ ******************************************************************************/
 package asap.realizerdemo;
 
 import java.io.IOException;
